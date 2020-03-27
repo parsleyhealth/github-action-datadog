1e688ad5effcac1784d6f64ef15e8a3440be1dd3
David Andersen <davidx@gmail.com>
2020-03-27 14:16:02 -0700

    initial

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const github = require('@actions/github');
 try {
     // `who-to-greet` input defined in action metadata file
     const key = core.getInput('datadog_api_key');
-    console.log(`Hello ${key}}!`);
+    const datadog_api_key = core.getInput('datadog_api_key');
+    const parsley_environment = core.getInput('parsley_environment');
+    const parsley_componentname = core.getInput('parsley_componentname');
+    const event_title = core.getInput('event_title');
+    const event_text = core.getInput('event_text');
+    const event_priority = core.getInput('event_priority');
+    const alert_type = core.getInput('alert_type');
+
+
+    console.log(`Hello ${key} ${datadog_api_key} ${parsley_componentname}   ${parsley_environment}  ${event_title} ${event_text} ${event_priority} ${alert_type} `);
     const time = (new Date()).toTimeString();
     core.setOutput("datadog_response", key);
     // Get the JSON webhook payload for the event that triggered the workflow
