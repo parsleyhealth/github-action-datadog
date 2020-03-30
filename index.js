dee9ded2c870a2cdbebce990cff44ba875708d51
David Andersen <davidx@gmail.com>
2020-03-30 16:47:14 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import DateTimeFormat = Intl.DateTimeFormat;
+
 const core = require("@actions/core");
 const github = require("@actions/github");
 const httpm = require("@actions/http-client");
@@ -14,23 +16,13 @@ const run = async () => {
     // core.debug("github payload")
     //
     //   core.debug(payload)
-
+    const current_time = Math.round((new Date()).getTime() / 1000);
     const reponame = payload['full_name']
     const branchname = payload['ref'].split('/').pop();
     const gitsha = payload['after'] // confirm
     const gitauthor = payload['commits'][0]['author']['username']
-// ]github payload
-//   ##[debug]{
-//     ##[debug]  "after": "97b3bdc201a2cd1564faeed121acaefba96df1d6",
-//         ##[debug]  "base_ref": null,
-//         ##[debug]  "before": "1eeafd235b361ad68a24a32b76807de926041373",
-//         ##[debug]  "commits": [
-//       ##[debug]    {
-//       ##[debug]      "author": {
-//       ##[debug]        "email": "davidx@gmail.com",
-//           ##[debug]        "name": "David Andersen",
-//           ##[debug]        "username": "davidx"
-//       ##[debug]      },
+    const last_commit_timestamp = Math.round((Date.parse(payload['head_commit']['timestamp'])).getTime() / 1000);
+    const lead_time = (current_time - last_commit_timestamp);
 
     const event_title = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment
     const event_text = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment
@@ -46,8 +38,6 @@ const run = async () => {
         "gitsha" + gitsha + " ",
         "gitauthor" + gitauthor + " "
     ]
-
-    const current_time = Math.round((new Date()).getTime() / 1000);
 
     let datadog_metric_payload = JSON.stringify({
         series: [
@@ -70,6 +60,8 @@ const run = async () => {
     });
 
     core.debug(datadog_metric_payload)
+    core.debug(datadog_event_payload)
+
     const response = await http.post(datadog_uri, datadog_metric_payload);
     const body = await response.readBody();
     core.debug(body);
