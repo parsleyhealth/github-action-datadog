036667468b44f2672d35c6f4941012d517bd57dc
David Andersen <davidx@gmail.com>
2020-03-30 12:29:41 -0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const core = require('@actions/core');
-const http = require('http');
+const http = require('httpie');
+import { get, post } from 'httpie';
 
+try {
+    const { data } = await get('https://pokeapi.co/api/v2/pokemon/1');
+
+    // Demo: Endpoint will echo what we've sent
+    const res = await post('https://jsonplaceholder.typicode.com/posts', {
+        body: {
+            id: data.id,
+            name: data.name,
+            number: data.order,
+            moves: data.moves.slice(0, 6)
+        }
+    });
+
+    console.log(res.statusCode); //=> 201
+    console.log(res.data); //=> { id: 1, name: 'bulbasaur', number: 1, moves: [{...}, {...}] }
+} catch (err) {
+    console.error('Error!', err.statusCode, err.message);
+    console.error('~> headers:', err.headers);
+    console.error('~> data:', err.data);
 
 try {
     // `who-to-greet` input defined in action metadata file
