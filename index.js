cbb96808a6bc7ad9b439aa405f4c89bc4291e41a
David Andersen <davidx@gmail.com>
2020-03-30 16:56:37 -0700

    fix var

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const run = async () => {
     const parsley_componentname = 'cicd_test'
     const parsley_environment = 'staging'
 
-    const payload = await JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     core.debug("github payload")
     //
     core.debug(payload.ref)
