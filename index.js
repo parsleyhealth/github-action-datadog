e5b2ae32f1d7b644117d0d0c8fdf9412235f4c96
David Andersen <davidx@gmail.com>
2020-03-30 14:27:20 -0700

    sync

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const run = async () => {
   core.debug(`datadog_api_key: ${datadog_api_key}`);
 
   const http = new httpm.HttpClient("http-client-tests");
-  const googleUri = "https://api.parsleyhealth.com/ping";
-  const res = await http.get(googleUri);
-  const body = await res.readBody();
+  let res =  { name: 'foo' };
+  var response  = await http.postJson('https://httpbin.org/post', res);
+//  const googleUri = "https://api.parsleyhealth.com/ping";
+ // const res = await http.get(googleUri);
+  const body = await response.readBody();
   core.debug(body);
 };
 
