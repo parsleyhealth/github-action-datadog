f48deaf313efe3f92ab4e3ad3995e43fef511af2
David Andersen <davidx@gmail.com>
2020-03-30 17:07:46 -0700

    fix var

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const run = async () => {
     const branchname = ref_path.split("/").pop();
     const gitsha = payload.after // confirm
     const gitauthor = payload.commits[0]['author']['username']
-    const last_commit_timestamp = Math.round((Date.parse(payload.head_commit['timestamp'])).getTime() / 1000);
+    const head_commit_timestamp = Date.parse(payload.head_commit['timestamp'])
+    const last_commit_timestamp = Math.round(head_commit_timestamp.getTime() / 1000);
     const lead_time = (current_time - last_commit_timestamp);
 
     const event_title = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment
