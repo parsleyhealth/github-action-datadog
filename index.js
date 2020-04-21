23595a29f3ac44b4aee50ec42e0e891b642fa0c4
Martín Beauchamp <martinb@parsleyhealth.com>
2020-04-21 13:26:33 -0400

    check for undefined datadog key.

--- index.js
+++ index.js
@@ -10,91 +10,88 @@ const run = async () => {
     const parsley_componentname = core.getInput("parsley-componentname");
     const parsley_environment = core.getInput("parsley-environment");
 
+    if (datadog_api_key == undefined){
+        console.log(`Please set DATADOG_API_KEY_PRODUCTION or DATADOG_API_KEY_STAGING in GitHub -> repository -> Settings -> Secrets.`);
+        process.exitCode = 1;
+    }
+
     let context = await github.context;
 
-    // we can only get required data out of certain event objects types
-    if (context.eventName === 'push' || context.eventName === 'repository_dispatch') {
+    const payload = context.payload;
+    const current_time = parseInt(Math.round((new Date()).getTime() / 1000));
+    const reponame = payload.repository.full_name;
+    const ref_path = payload.ref;
+    const branchname = ref_path.split('/').pop();
+    const gitauthor = payload.commits[0]['author']['username'];
+    const head_commit_timestamp = Date.parse(payload.head_commit['timestamp']);
+    const last_commit_epoch = parseInt(Math.round(head_commit_timestamp / 1000));
+    const lead_time = (current_time - last_commit_epoch);
 
-        const payload = context.payload;
-        const current_time = parseInt(Math.round((new Date()).getTime() / 1000));
-        const reponame = payload.repository.full_name;
-        const ref_path = payload.ref;
-        const branchname = ref_path.split('/').pop();
-        #const gitauthor = payload.commits[0]['author']['username'];
-        const head_commit_timestamp = Date.parse(payload.head_commit['timestamp']);
-        const last_commit_epoch = parseInt(Math.round(head_commit_timestamp / 1000));
-        const lead_time = (current_time - last_commit_epoch);
+    const event_title = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment;
+    const event_text = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment;
+    const event_priority = "normal";
 
-        const event_title = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment;
-        const event_text = "Deploy event for " + parsley_componentname + " in env: " + parsley_environment;
-        const event_priority = "normal";
+    const http = new httpm.HttpClient("http-client-parsley-health-cicd");
 
-        const http = new httpm.HttpClient("http-client-parsley-health-cicd");
+    const parsley_tags = [
+        "environment:" + parsley_environment,
+        "componentname:" + parsley_componentname,
+        "reponame:" + reponame,
+        "branchname:" + branchname,
+        "gitauthor:" + gitauthor
+    ];
 
-        const parsley_tags = [
-            "environment:" + parsley_environment,
-            "componentname:" + parsley_componentname,
-            "reponame:" + reponame,
-            "branchname:" + branchname,
-            "gitauthor:" + gitauthor
-        ];
-
-        let datadog_lead_time_metric_payload = JSON.stringify({
-                series: [
-                {
-                metric: "com.parsleyhealth.cicd.deploy_lead_time",
-                points: [[current_time, lead_time]],
-                type: "count",
-                unit: "seconds",
-                interval: 20,
-                host: "cicd.parsleyhealth.com",
-                tags: parsley_tags
-                }
-                ]
-        });
-        let datadog_metric_payload = JSON.stringify({
-                series: [
-                    {
-                        metric: "com.parsleyhealth.cicd.deploy",
-                        points: [[current_time, 1]],
-                        type: "count",
-                        unit: "boolean",
-                        interval: 20,
-                        host: "cicd.parsleyhealth.com",
-                        tags: parsley_tags
-                    }
-                ]
-        });
-
-        let datadog_event_payload = JSON.stringify({
-            title: "Deploy event for " + parsley_componentname,
-            text: "Deploy event for " + parsley_componentname,
-            priority: "normal",
+    let datadog_lead_time_metric_payload = JSON.stringify({
+            series: [
+            {
+            metric: "com.parsleyhealth.cicd.deploy_lead_time",
+            points: [[current_time, lead_time]],
+            type: "count",
+            unit: "seconds",
+            interval: 20,
+            host: "cicd.parsleyhealth.com",
             tags: parsley_tags
-        });
+            }
+            ]
+    });
+    let datadog_metric_payload = JSON.stringify({
+            series: [
+                {
+                    metric: "com.parsleyhealth.cicd.deploy",
+                    points: [[current_time, 1]],
+                    type: "count",
+                    unit: "boolean",
+                    interval: 20,
+                    host: "cicd.parsleyhealth.com",
+                    tags: parsley_tags
+                }
+            ]
+    });
 
-        var response = await http.post(datadog_metric_uri, datadog_metric_payload);
-        var body1 = await response.readBody();
-        core.debug(body1);
+    let datadog_event_payload = JSON.stringify({
+        title: "Deploy event for " + parsley_componentname,
+        text: "Deploy event for " + parsley_componentname,
+        priority: "normal",
+        tags: parsley_tags
+    });
 
-        var response = await http.post(datadog_metric_uri, datadog_lead_time_metric_payload);
-        var body2 = await response.readBody();
-        core.debug(body2);
+    var response = await http.post(datadog_metric_uri, datadog_metric_payload);
+    var body1 = await response.readBody();
+    core.debug(body1);
 
-        var response = await http.post(datadog_event_uri, datadog_event_payload);
-        var body3 = await response.readBody();
-        core.debug(body3);
-        var output = {
-                datadog_deploy_metric: body1,
-                datadog_lead_time_metric: body2,
-                datadog_deploy_event: body3
-        }
-        core.setOutput("datadog-response", JSON.stringify(output))
+    var response = await http.post(datadog_metric_uri, datadog_lead_time_metric_payload);
+    var body2 = await response.readBody();
+    core.debug(body2);
+
+    var response = await http.post(datadog_event_uri, datadog_event_payload);
+    var body3 = await response.readBody();
+    core.debug(body3);
+    var output = {
+            datadog_deploy_metric: body1,
+            datadog_lead_time_metric: body2,
+            datadog_deploy_event: body3
     }
-
-    else {
-        core.setOutput("action-response", "This action only runs on a push or repository_dispatch event.")
-    }
+    core.setOutput("datadog-response", JSON.stringify(output))
 };
 
 run();
